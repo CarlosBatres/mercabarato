c46433262f54168c48a704df14cb5930572e4131
Michel Escobar <michelejnjn@gmail.com>
2015-05-18 17:51:51 -0430

    Inicial

---
+++ application/views/header.php
@@ -0,0 +1,48 @@
+<!--<header class="navbar navbar-inverse navbar-static-top navbar-custom">
+    <div class="container">
+        <div class="navbar-header">            
+            <a href="<?php echo site_url(); ?>" class="navbar-brand"> <img src="<?php echo assets_url('imgs/logo.png') ?>"/> </a>
+        </div>
+        <nav class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="<?php echo site_url(); ?>">Subastas</a></li>
+                <li><a href="<?php echo site_url(); ?>">Productos</a></li>                
+                <li><a href="<?php echo site_url(); ?>">Vendedores</a></li>                
+                <li><a href="<?php echo site_url(); ?>">Seguros</a></li>                
+            </ul>            
+        </nav>
+    </div>
+</header>-->
+
+<header>
+    <!-- *** TOP ***
+    _________________________________________________________ -->
+    <div id="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-5 contact">
+                    <p class="hidden-sm hidden-xs">Contact us on +420 777 555 333 or hello@universal.com.</p>
+                    <p class="hidden-md hidden-lg"><a href="#" data-animate-hover="pulse"><i class="fa fa-phone"></i></a>  <a href="#" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
+                    </p>
+                </div>
+                <div class="col-xs-7">
+                    <div class="social">
+                        <a href="#" class="external facebook" data-animate-hover="pulse"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="external gplus" data-animate-hover="pulse"><i class="fa fa-google-plus"></i></a>
+                        <a href="#" class="external twitter" data-animate-hover="pulse"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="email" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
+                    </div>
+
+                    <div class="login">
+                        <a href="#" data-toggle="modal" data-target="#login-modal"><i class="fa fa-sign-in"></i> <span class="hidden-xs text-uppercase">Sign in</span></a>
+                        <a href="customer-register.html"><i class="fa fa-user"></i> <span class="hidden-xs text-uppercase">Sign up</span></a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- *** TOP END *** -->
+
+</header>
