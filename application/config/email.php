86e48c7f2f5510e3c6eb295541a91b1def27e5bb
Michel Escobar <michelejcs@hotmail.com>
2015-07-20 16:43:35 -0430

    email settings

--- application/config/email.php
+++ application/config/email.php
@@ -2,8 +2,8 @@
 
 /*EMAIL CONFIG*/
 $config['protocol'] = 'smtp';
-$config['smtp_host'] = 'mail.micesc-dev.com';
-$config['smtp_user'] = 'administrator@micesc-dev.com';
-$config['smtp_pass'] = 'enIFInxheOyy';
+$config['smtp_host'] = 'mail.mercabarato.com';
+$config['smtp_user'] = 'envios@mercabarato.com';
+$config['smtp_pass'] = 'mercadobaratoenvios';
 $config['smtp_port'] = '25';
 $config['mailtype'] = 'html';
